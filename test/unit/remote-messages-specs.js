bbce5d6c5b1722e3436b08c9898cbe0f49fc32cf
Isaac Murchie <isaac.appium@gmail.com>
2019-07-25 10:21:42 -0400

    Remove useless tests

--- test/unit/remote-messages-specs.js
+++ test/unit/remote-messages-specs.js
@@ -5,22 +5,24 @@ import { MOCHA_TIMEOUT } from '../helpers/helpers';
 
 chai.should();
 
-describe('RemoteMessages#getRemoteCommand', function () {
+describe('RemoteMessages', function () {
   this.timeout(MOCHA_TIMEOUT);
 
   const remoteMessages = new RemoteMessages();
 
-  const commands = [
-    'setConnectionKey', 'connectToApp', 'setSenderKey', 'indicateWebView',
-    'sendJSCommand', 'callJSFunction', 'setUrl', 'enablePage', 'startTimeline',
-    'stopTimeline',
-  ];
-  for (const command of commands) {
-    it(`should be able to retrieve ${command} command`, function () {
-      const remoteCommand = remoteMessages.getRemoteCommand(command, {});
-      remoteCommand.should.be.an.instanceof(Object);
-      remoteCommand.__argument.should.exist;
-      remoteCommand.__selector.should.exist;
-    });
-  }
+  describe('getRemoteCommand', function () {
+    const commands = [
+      'setConnectionKey', 'connectToApp', 'setSenderKey', 'indicateWebView',
+      'sendJSCommand', 'callJSFunction', 'setUrl', 'enablePage', 'startTimeline',
+      'stopTimeline',
+    ];
+    for (const command of commands) {
+      it(`should be able to retrieve ${command} command`, function () {
+        const remoteCommand = remoteMessages.getRemoteCommand(command, {});
+        remoteCommand.should.be.an.instanceof(Object);
+        remoteCommand.__argument.should.exist;
+        remoteCommand.__selector.should.exist;
+      });
+    }
+  });
 });
