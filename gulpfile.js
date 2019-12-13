95e3c1f8421af5fbb85daaee048d76ea84420fa2
Isaac A. Murchie <isaac.appium@gmail.com>
2019-12-13 06:05:02 -0500

    chore: add tmp dir to skipped files for yaml linting

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,14 @@ const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-remote-debugger',
-  files: ['*.js', 'lib/**/*.js', 'bin/**/*.js', 'test/**/*.js', '!gulpfile.js'],
+  files: [
+    '*.js', 'lib/**/*.js', 'bin/**/*.js', 'test/**/*.js',
+    '!gulpfile.js'
+  ],
+  yaml: {
+    files: [
+      '**/.*.yml', '**/*.yml', '**/.*.yaml', '**/*.yaml',
+      '!test/**', '!node_modules/**', '!**/node_modules/**', '!tmp/**'
+    ],
+  }
 });
