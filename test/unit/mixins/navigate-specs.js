259fb238d7c39b7106b0fd9d12ae69e666d03351
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-06-19 12:26:01 -0700

    fix lint

--- test/unit/mixins/navigate-specs.js
+++ test/unit/mixins/navigate-specs.js
@@ -8,60 +8,60 @@ chai.use(chaiAsPromised);
 describe('navigate', function () {
   describe('isPageLoadingCompleted', function () {
     describe('default pageLoadStrategy', function () {
-      it('with complete readyState', async function () {
+      it('with complete readyState', function () {
         const remoteDebugger = new RemoteDebugger();
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
-      it('with interactive readyState', async function () {
+      it('with interactive readyState', function () {
         const remoteDebugger = new RemoteDebugger();
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(false);
       });
-      it('with loading readyState', async function () {
+      it('with loading readyState', function () {
         const remoteDebugger = new RemoteDebugger();
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('eager pageLoadStrategy', function () {
-      it('with complete readyState', async function () {
+      it('with complete readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'eager'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
-      it('with interactive readyState', async function () {
+      it('with interactive readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'Eager'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(true);
       });
-      it('with loading readyState', async function () {
+      it('with loading readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'eager'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('normal pageLoadStrategy', function () {
-      it('with complete readyState', async function () {
+      it('with complete readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'NorMal'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
-      it('with interactive readyState', async function () {
+      it('with interactive readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'normaL'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(false);
       });
-      it('with loading readyState', async function () {
+      it('with loading readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'normal'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('none pageLoadStrategy', function () {
-      it('with complete readyState', async function () {
+      it('with complete readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'none'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
-      it('with interactive readyState', async function () {
+      it('with interactive readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'noNe'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(true);
       });
-      it('with loading readyState', async function () {
+      it('with loading readyState', function () {
         const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'NONE'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(true);
       });
