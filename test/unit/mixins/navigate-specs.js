2df1ffc45d2035dffa1662eca901e6f2154e75d2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-06-19 13:12:29 -0700

    add types

--- test/unit/mixins/navigate-specs.js
+++ test/unit/mixins/navigate-specs.js
@@ -7,62 +7,64 @@ chai.use(chaiAsPromised);
 
 describe('navigate', function () {
   describe('isPageLoadingCompleted', function () {
+    const BUNDLE_ID = 'com.apple.mobilesafari'
+
     describe('default pageLoadStrategy', function () {
       it('with complete readyState', function () {
-        const remoteDebugger = new RemoteDebugger();
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
       it('with interactive readyState', function () {
-        const remoteDebugger = new RemoteDebugger();
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(false);
       });
       it('with loading readyState', function () {
-        const remoteDebugger = new RemoteDebugger();
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('eager pageLoadStrategy', function () {
       it('with complete readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'eager'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'eager'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
       it('with interactive readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'Eager'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'Eager'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(true);
       });
       it('with loading readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'eager'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'eager'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('normal pageLoadStrategy', function () {
       it('with complete readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'NorMal'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'NorMal'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
       it('with interactive readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'normaL'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'normaL'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(false);
       });
       it('with loading readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'normal'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'normal'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(false);
       });
     });
 
     describe('none pageLoadStrategy', function () {
       it('with complete readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'none'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'none'});
         remoteDebugger.isPageLoadingCompleted('complete').should.eql(true);
       });
       it('with interactive readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'noNe'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'noNe'});
         remoteDebugger.isPageLoadingCompleted('interactive').should.eql(true);
       });
       it('with loading readyState', function () {
-        const remoteDebugger = new RemoteDebugger({pageLoadStrategy: 'NONE'});
+        const remoteDebugger = new RemoteDebugger({bundleId: BUNDLE_ID, pageLoadStrategy: 'NONE'});
         remoteDebugger.isPageLoadingCompleted('loading').should.eql(true);
       });
     });
