d1fda8080afaafe4880c36e008bfd248d039965a
Mykola Mokhnach <mokhnach@gmail.com>
2024-07-25 09:48:08 +0200

    feat: Refactor mixins and improve their typing (#392)

--- test/unit/mixins/execute-specs.js
+++ test/unit/mixins/execute-specs.js
@@ -1,8 +1,6 @@
 import { MOCHA_TIMEOUT } from '../../helpers/helpers';
-import exec from '../../../lib/mixins/execute';
+import { executeAtom, executeAtomAsync, callFunction, execute } from '../../../lib/mixins/execute';
 import sinon from 'sinon';
-
-const { executeAtom, executeAtomAsync, callFunction, execute } = exec;
 
 describe('execute', function () {
   this.timeout(MOCHA_TIMEOUT);
@@ -19,12 +17,14 @@ describe('execute', function () {
       const ctx = {
         appIdKey: 'appId',
         pageIdKey: 'pageId',
+        log: {debug: () => {}},
         execute,
         rpcClient: {
           isConnected: true,
           send: () => ({hello: 'world'}),
         },
       };
+      ctx.requireRpcClient = () => ctx.rpcClient;
       const res = await executeAtom.call(ctx, 'find_element', ['css selector', '#id', {ELEMENT: 'foo'}]);
       res.should.eql({hello: 'world'});
     });
@@ -34,12 +34,14 @@ describe('execute', function () {
       const ctx = {
         appIdKey: 'appId',
         pageIdKey: 'pageId',
+        log: {debug: () => {}},
         execute,
         rpcClient: {
           isConnected: true,
           send: () => ({result: {objectId: 'fake-object-id'}}),
         },
       };
+      ctx.requireRpcClient = () => ctx.rpcClient;
       const sendSpy = sinon.spy(ctx.rpcClient, 'send');
       await executeAtomAsync.call(ctx, 'find_element', ['a', 'b', 'c'], ['frame-1'], ['frame-2']);
       const callArgs = sendSpy.firstCall.args;
@@ -52,6 +54,7 @@ describe('execute', function () {
       const ctx = {
         appIdKey: 'fakeAppId',
         pageIdKey: 'fakePageId',
+        log: {debug: () => {}},
         garbageCollectOnExecute: true,
         garbageCollect () { },
         rpcClient: {
@@ -63,6 +66,7 @@ describe('execute', function () {
         waitForDom () { },
         pageLoading: true,
       };
+      ctx.requireRpcClient = () => ctx.rpcClient;
       const sendSpy = sinon.spy(ctx.rpcClient, 'send');
       await callFunction.call(ctx, 'fake-object-id', 'fake_function', ['a', 'b', 'c']);
       sendSpy.firstCall.args[0].should.equal('Runtime.callFunctionOn');
