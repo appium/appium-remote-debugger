b66c171e9df36332dd1d946b7afa3ca3d23b2f41
Mykola Mokhnach <mokhnach@gmail.com>
2026-02-06 13:35:33 +0100

    fix: Introduce proper overloads for createRemoteDebugger

--- lib/index.ts
+++ lib/index.ts
@@ -2,14 +2,21 @@ import { RemoteDebugger, REMOTE_DEBUGGER_PORT } from './remote-debugger';
 import { RemoteDebuggerRealDevice } from './remote-debugger-real-device';
 import type { RemoteDebuggerRealDeviceOptions, RemoteDebuggerOptions } from './types';
 
-export function createRemoteDebugger<T extends boolean> (
-  opts: T extends true ? RemoteDebuggerRealDeviceOptions : RemoteDebuggerOptions,
-  realDevice: T
-): T extends true ? RemoteDebuggerRealDevice : RemoteDebugger {
+export function createRemoteDebugger (
+  opts: RemoteDebuggerRealDeviceOptions,
+  realDevice: true
+): RemoteDebuggerRealDevice;
+export function createRemoteDebugger (
+  opts: RemoteDebuggerOptions,
+  realDevice: false
+): RemoteDebugger;
+export function createRemoteDebugger (
+  opts: RemoteDebuggerRealDeviceOptions | RemoteDebuggerOptions,
+  realDevice: boolean
+): RemoteDebuggerRealDevice | RemoteDebugger {
   return realDevice
     ? new RemoteDebuggerRealDevice(opts as RemoteDebuggerRealDeviceOptions)
-    // @ts-ignore TS does not understand that
-    : new RemoteDebugger(opts);
+    : new RemoteDebugger(opts as RemoteDebuggerOptions);
 }
 
 export { RemoteDebugger, RemoteDebuggerRealDevice, REMOTE_DEBUGGER_PORT };
